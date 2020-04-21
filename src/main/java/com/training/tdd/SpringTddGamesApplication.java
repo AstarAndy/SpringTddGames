38e2f088ac1c6cbdec57629b30c328127dac50bc
Andrew Faust <ArjfAndy@Gmail.com>
2020-04-21 13:00:07 -0400

    Initial project set-up

---
+++ src/main/java/com/training/tdd/SpringTddGamesApplication.java
@@ -0,0 +1,13 @@
+package com.training.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTddGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTddGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/tdd/SpringTddGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTddGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
